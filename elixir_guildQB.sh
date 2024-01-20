e22e92c5bae2203f13e98cd9d686bc0e9ebca930
PAPPY-PPY <angotuka@outlook.jp>
2024-01-20 20:36:21 +0900

    delete comment out

--- elixir_guildQB.sh
+++ elixir_guildQB.sh
@@ -9,15 +9,15 @@ printf '\033[35m%s\033[m\n' '╚██████╔╝╚██████╔
 printf '\033[35m%s\033[m\n' ' ╚═════╝  ╚═════╝ ╚═╝╚══════╝╚═════╝      ╚══▀▀═╝ ╚═════╝'
 echo ''
 
-# printf '\033[32m%s\033[m\n' "その１：Dockerのインストール"
-# printf '\033[32m%s\033[m\n' "続けていいすか？みたいに聞かれるのでenterを叩く。こんなやつ　→　Press [ENTER] to continue or Ctrl-c to cancel."
+printf '\033[32m%s\033[m\n' "その１：Dockerのインストール"
+printf '\033[32m%s\033[m\n' "続けていいすか？みたいに聞かれるのでenterを叩く。こんなやつ　→　Press [ENTER] to continue or Ctrl-c to cancel."
 
-# sudo apt update
-# sudo apt install apt-transport-https ca-certificates curl software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-# sudo apt update
-# sudo apt install docker-ce -y
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce -y
 
 printf '\033[32m%s\033[m\n' "その２：dockerの設定ファイルを作成"
 touch Dockerfile
